25888deb7d2dff2c27b306c49313440c2767062b
Ryan Ho <apexial@aol.com>
2022-09-12 02:07:01 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css';
+import SignIn from './components/SignIn';
+import Chat from './components/Chat';
+import { auth } from './firebase.js';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+
+function App() {
+  const [user] = useAuthState(auth)
+  return (
+    <>
+      {user ? <Chat /> : <SignIn />}
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseApp = firebase.initializeApp({
+ apiKey: "AIzaSyBIdWtIN0DhDtxCeafj7hnIVwamexNe7nQ",
+ authDomain: "chatbot-aa5fa.firebaseapp.com",
+ projectId: "chatbot-aa5fa",
+ storageBucket: "chatbot-aa5fa.appspot.com",
+ messagingSenderId: "958570634471",
+ appId: "1:958570634471:web:6254611100fbc3eb961642",
+ measurementId: "G-00BDBJCPHE"
+});
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export {db, auth};
